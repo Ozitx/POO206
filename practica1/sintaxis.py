450509f1a34afbed6c5ecbf412b3e3f84a83a69d
Ozitx <123046490@upq.edu.mx>
2025-05-16 18:18:50 -0600

    Variables en Python

--- practica1/sintaxis.py
+++ practica1/sintaxis.py
@@ -7,5 +7,26 @@ de varias lineas
 en python """
 
 # 2. strings
-print("hola soy una cadena")
-print('yo soy otra')
+#print('yo soy otra')
+
+variable1= "hola soy una cadena"
+#print(len(variable1)) #len guarda el tamaño de la cadena incluyendo los espacios
+#print(variable1[2:5])
+#print(variable1[2:])
+#print(variable1[:5])
+
+#3. Variables
+
+x= "cynthia"
+x= 4
+x= 5.67
+print(x)
+
+x= int(3)
+y= float(3)
+z= str(3)
+
+print(x,y,z)
+print(type(x)) #lanza cual fue el ultimo tipo de dato que se guardó
+print(type(y))
+print(type(z))
