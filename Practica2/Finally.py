ba0210d5b7e872001c59f8aa5eb28a79e2158e7f
Ozitx <123046490@upq.edu.mx>
2025-05-21 19:16:59 -0600

    Finally

--- Practica2/Finally.py
+++
@@ -1,7 +0,0 @@
-try:
-    numero = int(input("Ingresa un número: "))
-    resultado = 2 / numero
-except (ValueError, ZeroDivisionError):
-    print("Error: ingresa un valor válido")
-finally:
-    print("Fin del intento :3")
