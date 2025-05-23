bce8f0c6fb4e1804f9362f5df01a461551af08d8
Ozitx <123046490@upq.edu.mx>
2025-05-22 20:18:42 -0600

    Años
    Código que verifica si el año ingresado es bisiesto o no

---
+++ Practica3/Años.py
@@ -0,0 +1,12 @@
+for i in range(3):
+    try:
+        año = int(input("\nIngresa un año: "))
+        if año < 0:
+            print("Error: El año no puede ser negativo")
+            break
+        if (año % 4 == 0 and año % 100 != 0) or (año % 400 == 0):
+            print(f"El año {año} es bisiesto")
+        else:
+            print(f"El año {año} no es bisiesto")
+    except ValueError:
+        print("Error: ingrese un año válido por favor :3")    
