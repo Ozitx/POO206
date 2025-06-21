f58db362462e7942f8c16090e6dc3a391559eea4
Ozitx <123046490@upq.edu.mx>
2025-06-20 18:31:18 -0600

    Funciones del formulario
    Desarrollamos todas las funciones de nuestro formulario con ayuda de python

--- FlaskProyect/app.py
+++ FlaskProyect/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template, request,url_for,flash,redirect
 from flask_mysqldb import MySQL
 import MySQLdb #Importación del MySQL
 
@@ -9,30 +9,23 @@ app.config['MYSQL_HOST']="localhost" #toma el 27001
 app.config['MYSQL_USER']="root" #Definir el usuario
 app.config['MYSQL_PASSWORD']="Cynthia13" #Definir la pw
 app.config['MYSQL_DB']="DBflask"
+app.secret_key= 'mysecretkey'
 #app.config['MYSQL_PORT']="3306" //usar solo en cambio de puerto
 
 #Declarar una variable de mysql pasando la estancia de MySQL de mi propia app
 mysql= MySQL(app)
 
-#ruta para probar la conexión a MySQL
-@app.route('/DBCheck')
-def DB_check():
-    try:
-        cursor= mysql.connection.cursor()
-        cursor.execute('Select 1')
-        return jsonify( {'status':'ok','message':'Conectado con exito'} ),200
-    except MySQLdb.MySQLError as e:
-        return jsonify( {'status':'error','message':str(e)} ),500
 
-#Ruta simple
+
+#Ruta simple / de inicio
 @app.route('/')
 def home():
-    return 'Hola Mundo FLASK'
+    return render_template('formulario.html')
 
-#Ruta con parámetros
-@app.route('/saludo/<nombre>')
-def saludar(nombre):
-    return 'Hola, '+nombre+'!!!'
+#Ruta de consulta
+@app.route('/consulta')
+def consulta():
+    return render_template('consulta.html')
 
 #Ruta try-catch
 @app.errorhandler(404)
@@ -43,16 +36,56 @@ def paginaNoE(e):
 def metodonoP(e):
     return 'Revisa el método del enío de tu ruta (GET o POST !!!)',405
 
-#Ruta doble
-@app.route('/usuario')
-@app.route('/usuaria')
-def dobleroute():
-    return 'soy el mismo recurso del servidor'
-
-#Ruta POST
-@app.route('/formulario',methods=['POST'])
-def formulario():
-    return 'soy un formulario :D'
+#ruta para probar la conexión a MySQL
+@app.route('/DBCheck')
+def DB_check():
+    try:
+        cursor= mysql.connection.cursor()
+        cursor.execute('Select 1')
+        return jsonify( {'status':'ok','message':'Conectado con exito'} ),200
+    except MySQLdb.MySQLError as e:
+        return jsonify( {'status':'error','message':str(e)} ),500
+    
+#Agregar nueva ruta para agregar albums
+#Ruta para el Insert
+@app.route('/guardarAlbum', methods=['POST'])
+def guardar():
+    
+    #Lista de errores
+    errores={}
+    
+    #Obtener los datos a insertar
+    Vtitulo= request.form.get('txtTitulo','').strip()
+    Vartista= request.form.get('txtArtista','').strip()
+    Vanio= request.form.get('txtAnio','').strip()
+    
+    if not Vtitulo:
+        errores['txtTitulo']= 'Nombre del album obligatorio'
+    if not Vartista:
+        errores['txtArtista']= 'Nombre del artista obligatorio'
+    if not Vanio:
+        errores['txtAnio']= 'Año es obligatorio'
+    elif not Vanio.isdigit() or int(Vanio) < 1800 or int(Vanio) > 2100:
+        errores['txtAnio']= 'Ingresa un año válido'
+        
+    if not errores:
+        #Intentamos ejecutar el inset
+        try:
+            cursor= mysql.connection.cursor()
+            cursor.execute('insert into BD_Albums(album, artista, anio) values(%s,%s,%s)', (Vtitulo, Vartista, Vanio)) #Inserciones de datos
+            mysql.connection.commit()
+            flash('Album se guardo en BD')
+            return redirect(url_for('home'))
+        
+        except Exception as e:
+                mysql.connection.rollback()
+                flash('Esta mal en algo, nimodo'+ str(e))
+                return redirect(url_for('home'))
+            
+        finally:
+            cursor.close()
+    
+    return render_template('formulario.html', errores=errores)
 
 if __name__ == '__main__':
     app.run(port=3000,debug=True)
